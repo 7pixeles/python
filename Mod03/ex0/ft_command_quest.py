7547ae3cc81c0150ffc15e7db8c0b0a4317fea12
Ayua Carreno <ayuacarreno@gmail.com>
2026-03-08 21:30:10 +0100

    Add mod03 ex0

---
+++ Mod03/ex0/ft_command_quest.py
@@ -0,0 +1,21 @@
+#!/usr/env/bin python3
+import sys
+
+
+if __name__ == "__main__":
+    print("=== Command Quest ===\n ")
+    name = sys.argv[0]
+    argc = len(sys.argv) - 1
+    i = 1
+
+    if argc == 0:
+        print("No arguments provided!")
+        print("Program name:", name)
+        print("Total arguments:", len(sys.argv))
+    elif argc > 0:
+        print("Program name:", name)
+        print("Arguments received:", argc)
+        while i <= argc:
+            print(f"Argument {i}: {sys.argv[i]}")
+            i += 1
+        print("Total arguments:", len(sys.argv))
