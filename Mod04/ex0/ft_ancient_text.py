d9745fbc854841411261d3645fc630e50c6fd4c0
ayucarreno <ayucarreno@MacBook-Air-de-ayucarreno.local>
2026-03-21 17:28:20 +0100

    delete 'with' method (forbidden)

--- Mod04/ex0/ft_ancient_text.py
+++ Mod04/ex0/ft_ancient_text.py
@@ -9,10 +9,10 @@ def recovery_files():
         try:
             file_source = sys.argv[1]
             print("\n[CONNECTING...]")
-            with open(file_source, 'r') as file:
-                print("Accessing Storage Vault:", file.name)
-                print("\nRECOVERED DATA:")
-                print(file.read())
+            file = open(file_source, 'r')
+            print("Accessing Storage Vault:", file.name)
+            print("\nRECOVERED DATA:")
+            print(file.read())
             file.close()
             print("\n[COMPLETED] Data recovery complete. Storage unit disconected")
         except FileNotFoundError:
