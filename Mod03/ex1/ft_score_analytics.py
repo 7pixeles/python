5bc2d793701e5172888c9a0fa312aa8f5ff66552
ayucarreno <ayucarreno@MacBook-Air-de-ayucarreno.local>
2026-04-11 18:27:38 +0200

    add ex1 mod3 fixed

--- Mod03/ex1/ft_score_analytics.py
+++ Mod03/ex1/ft_score_analytics.py
@@ -9,21 +9,32 @@ import sys
 
 if __name__ == "__main__":
     scores = sys.argv[1:]
+    valid_scores = []
 
-    print("=== Player Score Analytics === \n")
+    print("=== Player Score Analytics ===")
+
     if len(scores) == 0:
-        print("No scores provided. "
-              "Usage python3 ft_score_analytics.py <score1> <score2> ...")
+        print("No scores provided.", end=" ")
+        print("Usage: python3 ft_score_analytics.py <score1> <score2> ...")
     else:
-        try:
-            int_scores = [int(score) for score in scores]
-            print("Scores processed:", int_scores)
-            print("Total players:", len(int_scores))
-            print("Total score:", sum(int_scores))
-            print("Average score:", sum(int_scores) / len(int_scores))
-            print("High score:", max(int_scores))
-            print("Low score:", min(int_scores))
-            print("Score range:", max(int_scores) - min(int_scores))
+        i = 0
+        while i < len(scores):
+            try:
+                num = int(scores[i])
+                valid_scores.append(num)
+            except ValueError:
+                print(f"Invalid parameter: '{scores[i]}'")
+            i += 1
 
-        except ValueError as error:
-            print("Error:", error)
+        if len(valid_scores) == 0:
+            print("No scores provided.", end=" ")
+            print("Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+        else:
+            total = sum(valid_scores)
+            print("Scores processed:", valid_scores)
+            print("Total players:", len(valid_scores))
+            print("Total score:", total)
+            print("Average score:", total / len(valid_scores))
+            print("High score:", max(valid_scores))
+            print("Low score:", min(valid_scores))
+            print("Score range:", max(valid_scores) - min(valid_scores))
